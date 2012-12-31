21dde08e578eff5cfb160764700d1c6e900faf5f
liquid0624 <rjbirnesser@gmail.com>
2012-12-31 14:43:18 -0500

    Cleanup Grouper Device/Implement Optimizations/Updates From CM

--- liquid.mk
+++ liquid.mk
@@ -1,18 +1,11 @@
 # Release name
 PRODUCT_RELEASE_NAME := Nexus7
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 800
-
 # Inherit some common stuff.
 $(call inherit-product, vendor/liquid/config/common_tablet.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/grouper/full_grouper.mk)
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dexopt-data-only=1
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grouper
@@ -22,4 +15,4 @@ PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.2/JOP40C/527662:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2 JOP40C 527662 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.2.1/JOP40D/533553:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2.1 JOP40D 533553 release-keys"
