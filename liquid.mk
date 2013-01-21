1781473e409ba167a3d9606439bb19b587498e2d
Matt Bloomfield <mbloomfield87@gmail.com>
2013-01-21 10:37:04 +0000

    Pull Request - As Per Liquid
    # needed for boot & # locales added back in.

--- liquid.mk
+++ liquid.mk
@@ -16,3 +16,10 @@ PRODUCT_MANUFACTURER := Asus
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.2.1/JOP40D/533553:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2.1 JOP40D 533553 release-keys"
+
+# locales
+PRODUCT_LOCALES := cs_CZ da_DK de_AT de_CH de_DE de_LI el_GR en_AU en_CA en_GB en_NZ en_SG en_US es_ES fr_CA fr_CH fr_BE fr_FR it_CH it_IT ja_JP ka_GE ko_KR nb_NO nl_BE nl_NL nn_NO pl_PL pt_PT ru_RU sv_SE tr_TR zh_CN zh_HK zh_TW am_ET hi_IN
+
+# need for boot
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.dexopt-data-only=1
