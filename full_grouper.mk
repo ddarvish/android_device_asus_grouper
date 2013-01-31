0db68b35a01a7ae77de4bed32904a2d7e46cd3c4
slickrick <rsalinas1@mail.usf.edu>
2013-01-31 16:12:55 -0500

    More Carbon

--- full_grouper.mk
+++ full_grouper.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2012 The CyanogenMod Project
-# Copyright (C) 2012 The LiquidSmooth Project
+# Copyright (C) 2012 The Carbon Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ $(call inherit-product, device/asus/grouper/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # product
-PRODUCT_NAME := liquid_grouper
+PRODUCT_NAME := carbon_grouper
 PRODUCT_DEVICE := grouper
 PRODUCT_BRAND := Google
 PRODUCT_MANUFACTURER := Asus
